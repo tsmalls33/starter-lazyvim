e9348878ada59348506ebc9a84d30fee7aac720f
Thomas Smallwood <thomas.smallwood@atomlearning.co.uk>
2025-11-30 16:52:26 +0000

    toggle term session fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,8 +7,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Cursor in middle when jumping
-vim.keymap.set("n", "<C-j", "<C-d>zz")
-vim.keymap.set("n", "<C-k", "<C-u>zz")
+vim.keymap.set("n", "<C-j>", "<C-d>zz")
+vim.keymap.set("n", "<C-k>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
