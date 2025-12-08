6fa39301cd725ede8e038e2b8010540ad3789aa8
Thomas Smallwood <tbm.smallwood@gmail.com>
2025-12-08 17:05:26 +0000

    prettier formatting using project .yarn bin file

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,26 @@
+local util = require("conform.util")
+
 return {
   "stevearc/conform.nvim",
   opts = {
+    formatters = {
+      prettier = {
+        command = util.from_node_modules(".yarn/sdks/prettier/bin/prettier.cjs"),
+      },
+      eslint = {
+        -- Run project ESLint through Yarn
+        command = "yarn",
+        args = { "eslint", "--fix-dry-run", "--stdin", "--stdin-filename", "$FILENAME" },
+        stdin = true,
+        cwd = util.root_file({ "eslint.config.mjs", ".pnp.cjs", "package.json" }),
+        timeout = 10000, -- 10s; adjust if needed
+      },
+    },
     formatters_by_ft = {
-      typescript = { "prettier" }, -- no Prettier for TS
+      typescript = { "prettier" },
       typescriptreact = { "prettier" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
     },
   },
 }
